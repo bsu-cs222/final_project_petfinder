2b4b05f061c8485750bd825b9e35692fa0471c19
Destiny J. Wells <djwells1113@gmail.com>
2023-10-15 14:34:26 -0400

    Unit Test 1 - Failing

--- test/unit_test.dart
+++ test/unit_test.dart
@@ -1,9 +1,10 @@
 import 'package:cs222_final_project_pet_finder/pet_finder_parser.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() async{
-  test ('When I enter 47306 I find data for the pets nearby', () async{
+  test (' When I enter 47306, I find data for the pets nearby', () async{
     final parser = PetFinderParser();
     final result = await parser.makeRequestToAPI();
-    expect(result, 'Rabbit');
+    expect(result, '47306');
   });
 }
