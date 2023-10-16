b067ed0158d5c3957450da9be8ea2100da2285a1
DJWtheCoder <djwells1113@gmail.com>
2023-10-16 06:23:11 -0400

    Some clean up

--- lib/exploratory_code.dart
+++ lib/exploratory_code.dart
@@ -1,9 +1,7 @@
 import 'package:cs222_final_project_pet_finder/pet_finder_parser.dart';
 
 void main() async{
-  test ('When I enter 47306 I find data for the pets nearby', () async{
     final parser = PetFinderParser();
     final result = await parser.makeRequestToAPI();
-    expect(result, 'Rabbit');
-  });
+    print(result);
 }
