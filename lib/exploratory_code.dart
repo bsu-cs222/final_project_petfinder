92a7cc9d53da92aec7a97fc7421a329f5878e105
TierraCodes <112350897+TierraCodes@users.noreply.github.com>
2023-10-16 06:28:43 -0400

    refactored for more specific test

--- lib/exploratory_code.dart
+++ lib/exploratory_code.dart
@@ -1,7 +1,9 @@
 import 'package:cs222_final_project_pet_finder/pet_finder_parser.dart';
+import 'package:flutter_test/flutter_test.dart';
 
-void main() async{
-  test ('When I enter 47306 I find data for the pets nearby', () async{
+void main() async {
+  test('When I enter the zipcode 47306 I find data for the pets nearby',
+      () async {
     final parser = PetFinderParser();
     final result = await parser.makeRequestToAPI();
     expect(result, 'Rabbit');
