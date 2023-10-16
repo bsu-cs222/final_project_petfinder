7107e8354852c771b807c81687dcead1c3286a05
TierraCodes <112350897+TierraCodes@users.noreply.github.com>
2023-10-16 07:24:06 -0400

    started work on parsing image urls(not working)

--- lib/pet_finder_parser.dart
+++ lib/pet_finder_parser.dart
@@ -1,37 +1,44 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-class PetFinderParser{
-    Future<String> makeRequestToAPI() async{
-      const String clientId = 'WP27Mq8UDXUpKodCbHgDRHGIvTaz7pTfTTHPPP8ZBzOJrlO3AZ';
-      const String clientSecret = 'Ana1nynvKNyQtKbJAm5mTQISlrtaaYeA8ao4gjm3';
-      const String apiUrl = 'https://api.petfinder.com/v2/oauth2/token';
+class PetFinderParser {
+  Future<String> makeRequestToAPI() async {
+    const String clientId =
+        'WP27Mq8UDXUpKodCbHgDRHGIvTaz7pTfTTHPPP8ZBzOJrlO3AZ';
+    const String clientSecret = 'Ana1nynvKNyQtKbJAm5mTQISlrtaaYeA8ao4gjm3';
+    const String apiUrl = 'https://api.petfinder.com/v2/oauth2/token';
 
-      final Map<String, String> requestBody = {
-        'grant_type': 'client_credentials',
-        'client_id': clientId,
-        'client_secret': clientSecret,
-      };
+    final Map<String, String> requestBody = {
+      'grant_type': 'client_credentials',
+      'client_id': clientId,
+      'client_secret': clientSecret,
+    };
 
-      final response = await http.post(
-        Uri.parse(apiUrl),
-        body: requestBody,
+    final response = await http.post(
+      Uri.parse(apiUrl),
+      body: requestBody,
+    );
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> data = json.decode(response.body);
+      final String accessToken = data['access_token'];
+      final queryResponse = await http.get(
+        Uri.parse('https://api.petfinder.com/v2/animals/?limit=1&type=rabbit'),
+        headers: {
+          'Authorization': 'Bearer $accessToken',
+        },
       );
-
-      if (response.statusCode == 200) {
-        final Map<String, dynamic> data = json.decode(response.body);
-        final String accessToken = data['access_token'];
-        final queryResponse = await http.get(
-          Uri.parse('https://api.petfinder.com/v2/animals/?limit=1&type=rabbit'),
-          headers: {
-            'Authorization': 'Bearer $accessToken',
-          },
-        );
-        final Map<String, dynamic> apiResponse = json.decode(queryResponse.body);
-        final animal = apiResponse['animals'][0]['species'];
-        return(animal);
-      } else {
-        return('Error: ${response.statusCode}');
-      }
+      final Map<String, dynamic> apiResponse = json.decode(queryResponse.body);
+      final animal = apiResponse['animals'][0]['species'];
+      return (animal);
+    } else {
+      return ('Error: ${response.statusCode}');
     }
+  }
+
+  String image() {
+    final Map<String, dynamic> apiResponse = json.decode(queryResponse.body);
+    final imageUrl = apiResponse['animals'][5]['photos']['small'];
+    return (imageUrl);
+  }
 }
--- test/unit_test.dart
+++ test/unit_test.dart
@@ -1,16 +1,24 @@
 import 'package:cs222_final_project_pet_finder/pet_finder_parser.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-void main() async{
-  test (' When I enter 47306, I find data for the pets nearby', () async{
+void main() async {
+  test(' When I enter 47306, I find data for the pets nearby', () async {
     final parser = PetFinderParser();
     final result = await parser.makeRequestToAPI();
     expect(result, '47306');
   });
   // This is going to decode the JSON into variables
-  test ('When I enter Indiana, I receive the JSON code for the top five pets in the area', () async{
+  test(
+      'When I enter Indiana, I receive the JSON code for the top five pets in the area',
+      () async {
     final parser = PetFinderParser();
     final result = await parser.makeRequestToAPI();
     expect(result, '47306');
+  });
+  test('I can display an image url', () {
+    final parser = PetFinderParser();
+    final result = parser.image();
+    expect(result,
+        'https://photos.petfinder.com/photos/pets/42706540/1/?bust=1546042081&width=100');
   });
 }
