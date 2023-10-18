bff26b480578dd1ac74d42fce975b40ee23c98b6
DJWtheCoder <djwells1113@gmail.com>
2023-10-18 14:42:41 -0400

    Unit Test 2 - Refactoring

--- lib/exploratory_code.dart
+++ lib/exploratory_code.dart
@@ -2,11 +2,10 @@ import 'package:cs222_final_project_pet_finder/pet_finder_parser.dart';
 import 'dart:io';
 
 void main() {
-    // final parser = PetFinderParser();
+    final parser = PetFinderParser();
     // final result = await parser.makeRequestToAPI();
     // print(result);
 
-    final parser = PetFinderParser();
     final File petTestFile = File('test/apiResponse.json');
     final fileContents = petTestFile.readAsStringSync();
     final results = parser.findFive(fileContents);
--- test/unit_test.dart
+++ test/unit_test.dart
@@ -1,19 +1,22 @@
 import 'dart:io';
 import 'package:cs222_final_project_pet_finder/pet_finder_parser.dart';
 import 'package:flutter_test/flutter_test.dart';
+final parser = PetFinderParser();
+final File petTestFile = File('test/apiResponse.json');
+final fileContents = petTestFile.readAsStringSync();
 
 void main() async{
   test (' When I enter 47306, I find data for the pets nearby', () async{
-    final parser = PetFinderParser();
     final result = await parser.makeRequestToAPI();
     expect(result, '47306');
   });
-  // This is going to decode the JSON into variables
+
   test ('Return the names of five pets from query response', () {
-    final parser = PetFinderParser();
-    final File petTestFile = File('test/apiResponse.json');
-    final fileContents = petTestFile.readAsStringSync();
     final results = parser.findFive(fileContents);
-    expect(results.length, 5);
+    var petNames = '';
+    for (var result in results){
+      petNames += '${result.name}, ';
+    }
+    expect(petNames, 'Bones, Charm, Count Coolman, Tara, Salem, ');
   });
 }
