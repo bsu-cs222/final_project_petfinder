de768d0965566830b0d00a7f86916acad99145b1
djwthecoder <djwells1113@gmail.com>
2023-10-21 15:38:25 -0400

    Intense refactoring

--- lib/exploratory_code.dart
+++ lib/exploratory_code.dart
@@ -1,16 +1,16 @@
 import 'package:cs222_final_project_pet_finder/pet_finder_parser.dart';
 import 'dart:io';
 
-void main() {
+void main() async{
     final parser = PetFinderParser();
     // final result = await parser.makeRequestToAPI();
     // print(result);
 
     final File petTestFile = File('test/apiResponse.json');
     final fileContents = petTestFile.readAsStringSync();
-    final results = parser.findFive(fileContents);
+    final results = parser.parseFivePets(fileContents);
 
     for (var result in results){
-        print('Pet Name: ${result.name}, Species: ${result.species}, Breed: ${result.breed}');
+        print('${result.name} is a ${result.breed} ${result.species}.');
     }
 }
--- lib/pet_finder_parser.dart
+++ lib/pet_finder_parser.dart
@@ -9,48 +9,56 @@ class Pet {
   Pet({required this.name, required this.species, required this.breed});
 }
 
-class PetFinderParser{
-    Future<String> makeRequestToAPI() async{
-      const String clientId = 'WP27Mq8UDXUpKodCbHgDRHGIvTaz7pTfTTHPPP8ZBzOJrlO3AZ';
-      const String clientSecret = 'Ana1nynvKNyQtKbJAm5mTQISlrtaaYeA8ao4gjm3';
-      const String apiUrl = 'https://api.petfinder.com/v2/oauth2/token';
+class QueryBuilder{
+  Map<String, String> tokenQueryBuilder() {
+    const String clientId = 'WP27Mq8UDXUpKodCbHgDRHGIvTaz7pTfTTHPPP8ZBzOJrlO3AZ';
+    const String clientSecret = 'Ana1nynvKNyQtKbJAm5mTQISlrtaaYeA8ao4gjm3';
 
-      final Map<String, String> requestBody = {
-        'grant_type': 'client_credentials',
-        'client_id': clientId,
-        'client_secret': clientSecret,
-      };
+    final Map<String, String> requestBody = {
+      'grant_type': 'client_credentials',
+      'client_id': clientId,
+      'client_secret': clientSecret,
+    };
 
-      final response = await http.post(
-        Uri.parse(apiUrl),
-        body: requestBody,
+    return(requestBody);
+  }
+
+  Map<String, String> PetFinderCallBuilder(tokenRequestResponse){
+    final Map<String, dynamic> decodedTokenRequestResponse = json.decode(tokenRequestResponse.body);
+    final String accessToken = decodedTokenRequestResponse['access_token'];
+    final Map<String, String> header = {
+      'Authorization': 'Bearer $accessToken',
+    };
+    return(header);
+  }
+}
+class QueryCall{
+  Future<Object> makeRequestToAPI() async{
+    final query = QueryBuilder();
+
+    final response = await http.post(
+      Uri.parse('https://api.petfinder.com/v2/oauth2/token'),
+      body: query.tokenQueryBuilder(),
+    );
+    if (response.statusCode == 200) {
+      final queryResponse = await http.get(
+        Uri.parse('https://api.petfinder.com/v2/animals/?limit=1&type=rabbit'),
+        headers: query.PetFinderCallBuilder(response),
       );
-
-      if (response.statusCode == 200) {
-        final Map<String, dynamic> data = json.decode(response.body);
-        final String accessToken = data['access_token'];
-        final queryResponse = await http.get(
-          Uri.parse('https://api.petfinder.com/v2/animals/?limit=1&type=rabbit'),
-          headers: {
-            'Authorization': 'Bearer $accessToken',
-          },
-        );
-        final Map<String, dynamic> apiResponse = json.decode(queryResponse.body);
-        final animal = apiResponse['animals'][0]['species'];
-        return(animal);
-      } else {
-        return('Error: ${response.statusCode}');
-      }
+      return(queryResponse);
+    } else {
+      return('Error: ${response.statusCode}');
     }
+  }
+}
 
-    List findFive(queryResponse) {
+class PetFinderParser{
+    List parseFivePets(queryResponse) {
       final decodedAPIResponse = json.decode(queryResponse);
       final listOfReturnedAnimals = decodedAPIResponse['animals'];
-
       List<Pet> pets = List<Pet>.generate(5, (index){
         return Pet(name: listOfReturnedAnimals[index]['name'], species:listOfReturnedAnimals[index]['species'], breed: listOfReturnedAnimals[index]['breeds']['primary']);
       });
-
       return pets;
     }
 }
