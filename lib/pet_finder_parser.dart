2e38c02e04860cfa16649ac71a033db6d5b0bafd
megawyatts <sarasaurus101@gmail.com>
2023-10-28 10:13:18 -0400

    Zipcode foundations: Green I guess 10:13 am-Sol

--- lib/pet_finder_parser.dart
+++ lib/pet_finder_parser.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:cs222_final_project_pet_finder/main.dart';
 import 'package:http/http.dart' as http;
 
 class Pet {
@@ -7,13 +8,15 @@ class Pet {
   final String breed;
   final String URLString;
   final List photos;
+  final zipcode;
 
   Pet(
       {required this.name,
       required this.species,
       required this.breed,
       required this.URLString,
-      required this.photos});
+      required this.photos,
+      required this.zipcode});
 }
 
 class QueryBuilder {
@@ -67,14 +70,15 @@ class PetFinderParser {
   List parseFivePets(queryResponse) {
     final decodedAPIResponse = json.decode(queryResponse);
     final listOfReturnedAnimals = decodedAPIResponse['animals'];
-
     List<Pet> pets = List<Pet>.generate(5, (index) {
+      print(listOfReturnedAnimals[index]['contact']['address']['postcode']);
       return Pet(
         name: listOfReturnedAnimals[index]['name'],
         species: listOfReturnedAnimals[index]['species'],
         breed: listOfReturnedAnimals[index]['breeds']['primary'],
         URLString: listOfReturnedAnimals[index]['url'],
         photos: listOfReturnedAnimals[index]['photos'],
+        zipcode: listOfReturnedAnimals[index]['contact']['address']['postcode'],
       );
     });
 
