650b275dc110e348466444e0eada9854cbf028d1
Solstice Wozniak <122378464+megawyatts@users.noreply.github.com>
2023-11-30 08:11:26 -0500

    Got rid of if-else statement that was unesscesary

--- lib/input_wizard.dart
+++ lib/input_wizard.dart
@@ -6,11 +6,12 @@ class InputWizard{
   String organizeGenderInput(TextEditingController genderController){
     String genderInputBody=genderController.text;
     String genderResult=inputEvaluator.inspectGenderInput(genderInputBody);
-    if(genderResult=='Error'){
-      return ('male,female'); //COME BACK because eventually we'll be running the method builder from here, WE CAN'T DO THAT NOW CAUSE DESTINY IS DOING METHOD CALLS SO UNTIL THEN WE RUN IT LIKE THIS -Sol
-    }
-    else{
-    return(genderResult);}
+    return(genderResult);
 
+  }
+  String organizeAgeInput(TextEditingController ageController){
+    String ageInputBody=ageController.text;
+    String ageResult=inputEvaluator.inspectAgeInput(ageInputBody);
+    return (ageResult);
   }
 }
