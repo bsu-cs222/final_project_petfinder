63ee638b9c2bd60794dc2b0a38c2b0c7b2ee49d1
TierraCodes <112350897+TierraCodes@users.noreply.github.com>
2023-12-06 09:00:54 -0500

    Refactor
    Refactored input_evaluator, input_wizard, and pet_finder_parser to follow CC rules

--- lib/input_wizard.dart
+++ lib/input_wizard.dart
@@ -1,27 +1,29 @@
 import 'package:cs222_final_project_pet_finder/input_evaluator.dart';
 import 'package:flutter/material.dart';
 
-class InputWizard{
+class InputWizard {
   final inputEvaluator = InputEvaluator();
-  bool organizeZipcodeInput(TextEditingController zipcodeController){
-    String zipcodeInputBody=zipcodeController.text;
-    bool zipcodeResult=inputEvaluator.inspectZipcodeInput(zipcodeInputBody);
-    return(zipcodeResult);
+  bool organizeZipcodeInput(TextEditingController zipcodeController) {
+    String zipcodeInputBody = zipcodeController.text;
+    bool zipcodeResult = inputEvaluator.inspectZipcodeInput(zipcodeInputBody);
+    return (zipcodeResult);
   }
-  String organizeGenderInput(TextEditingController genderController){
-    String genderInputBody=genderController.text;
-    String genderResult=inputEvaluator.inspectGenderInput(genderInputBody);
-    return(genderResult);
 
+  String organizeGenderInput(TextEditingController genderController) {
+    String genderInputBody = genderController.text;
+    String genderResult = inputEvaluator.inspectGenderInput(genderInputBody);
+    return (genderResult);
   }
-  String organizeAgeInput(TextEditingController ageController){
-    String ageInputBody=ageController.text;
-    String ageResult=inputEvaluator.inspectAgeInput(ageInputBody);
+
+  String organizeAgeInput(TextEditingController ageController) {
+    String ageInputBody = ageController.text;
+    String ageResult = inputEvaluator.inspectAgeInput(ageInputBody);
     return (ageResult);
   }
-  String organizeSpeciesInput(TextEditingController speciesController){
-    String speciesInputBody=speciesController.text;
-    String speciesResult=inputEvaluator.inspectSpeciesInput(speciesInputBody);
+
+  String organizeSpeciesInput(TextEditingController speciesController) {
+    String speciesInputBody = speciesController.text;
+    String speciesResult = inputEvaluator.inspectSpeciesInput(speciesInputBody);
     return (speciesResult);
   }
 }
--- lib/pet_finder_parser.dart
+++ lib/pet_finder_parser.dart
@@ -1,42 +1,44 @@
 import 'dart:convert';
 import 'package:cs222_final_project_pet_finder/pet.dart';
 
-
 class PetFinderParser {
   List parsePetInfo(queryResponse) {
     final decodedAPIResponse = json.decode(queryResponse);
     final listOfReturnedAnimals = decodedAPIResponse['animals'];
     List<Pet> pets = List<Pet>.generate(listOfReturnedAnimals.length, (index) {
       return Pet(
-        name: listOfReturnedAnimals[index]['name'],
-        species: evaluateSpecies(listOfReturnedAnimals[index]['species']),
-        breed: listOfReturnedAnimals[index]['breeds']['primary'],
-        urlString: listOfReturnedAnimals[index]['url'],
-        photos: listOfReturnedAnimals[index]['photos'],
-        zipcode: listOfReturnedAnimals[index]['contact']['address']['postcode'],
-        gender: evaluateGender(listOfReturnedAnimals[index]['gender']),
-        age: evaluateAge(listOfReturnedAnimals[index]['age']),
-        petID: listOfReturnedAnimals[index]['id'],
-        favPet: false
-      );
+          name: listOfReturnedAnimals[index]['name'],
+          species: evaluateSpecies(listOfReturnedAnimals[index]['species']),
+          breed: listOfReturnedAnimals[index]['breeds']['primary'],
+          urlString: listOfReturnedAnimals[index]['url'],
+          photos: listOfReturnedAnimals[index]['photos'],
+          zipcode: listOfReturnedAnimals[index]['contact']['address']
+              ['postcode'],
+          gender: evaluateGender(listOfReturnedAnimals[index]['gender']),
+          age: evaluateAge(listOfReturnedAnimals[index]['age']),
+          petID: listOfReturnedAnimals[index]['id'],
+          favPet: false);
     });
     return pets;
   }
-  int parseAdoption(queryResponse){
+
+  int parseAdoption(queryResponse) {
     final decodedAPIResponse = json.decode(queryResponse);
     final totalOfAnimal = decodedAPIResponse['pagination']['total_count'];
     return totalOfAnimal;
   }
+
   GenderType evaluateGender(petListedGender) {
     switch (petListedGender) {
       case 'Male':
         return GenderType.male;
       default:
         return GenderType.female;
+    }
+  }
 
-  }}
-  AgeType evaluateAge(petListedAge){
-    switch(petListedAge){
+  AgeType evaluateAge(petListedAge) {
+    switch (petListedAge) {
       case 'Baby':
         return AgeType.baby;
       case 'Young':
@@ -47,15 +49,16 @@ class PetFinderParser {
         return AgeType.senior;
     }
   }
-  SpeciesType evaluateSpecies(petListedSpecies){
-    switch(petListedSpecies){
-      case'Dog':
+
+  SpeciesType evaluateSpecies(petListedSpecies) {
+    switch (petListedSpecies) {
+      case 'Dog':
         return SpeciesType.dog;
-      case'Cat':
+      case 'Cat':
         return SpeciesType.cat;
       case 'Small-furry' || 'Rat' || 'Gerbil' || 'Guinea Pig':
         return SpeciesType.rodent;
-      case'Barnyard' || 'Goat' || 'Pot Bellied':
+      case 'Barnyard' || 'Goat' || 'Pot Bellied':
         return SpeciesType.barnyard;
       case 'Bird':
         return SpeciesType.bird;
