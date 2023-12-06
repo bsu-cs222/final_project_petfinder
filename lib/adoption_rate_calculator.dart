15ea06d82d24429fe8bbf98bf79d3cd984bb81ce
TierraCodes <112350897+TierraCodes@users.noreply.github.com>
2023-12-06 08:53:06 -0500

    Refactor
    Refactored adoption_rate_calculator and enum_decoder to follow CC rules

--- lib/adoption_rate_calculator.dart
+++ lib/adoption_rate_calculator.dart
@@ -4,26 +4,25 @@ import 'package:cs222_final_project_pet_finder/pet_finder_parser.dart';
 import 'package:cs222_final_project_pet_finder/query_builder.dart';
 
 class AdoptionRateCalculator {
-  int calculateRate(int adopted, int adoptable) {
-    double rateAsDecimal = (adopted / (adopted + adoptable)) * 100;
-    final rateAsPercentage = rateAsDecimal.round();
-    return (rateAsPercentage);
-  }
-
   Future<int> returnFinalRate(Pet pet) async {
     final caller = APICaller();
     final query = QueryBuilder();
     final parser = PetFinderParser();
 
-    final adopted = await caller.makeRequestToAPI(
-        query.pullAdoptedFound(pet));
+    final adopted = await caller.makeRequestToAPI(query.pullAdoptedFound(pet));
     final parsedAdopted = parser.parseAdoption(adopted);
 
-    final adoptable = await caller.makeRequestToAPI(
-        query.pullAdoptableFound(pet));
+    final adoptable =
+        await caller.makeRequestToAPI(query.pullAdoptableFound(pet));
     final parsedAdoptable = parser.parseAdoption(adoptable);
 
     final yearlyRate = calculateRate(parsedAdopted, parsedAdoptable);
     return (yearlyRate);
+  }
+
+  int calculateRate(int adopted, int adoptable) {
+    double rateAsDecimal = (adopted / (adopted + adoptable)) * 100;
+    final rateAsPercentage = rateAsDecimal.round();
+    return (rateAsPercentage);
   }
 }
