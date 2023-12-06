19faacffcd5433a6159a8108387d0ff1ef454874
TierraCodes <112350897+TierraCodes@users.noreply.github.com>
2023-12-06 07:58:36 -0500

    Refactor
    Refactored for CC

--- test/idreader_test.dart
+++ test/idreader_test.dart
@@ -1,27 +1,27 @@
 import 'dart:io';
-import 'package:cs222_final_project_pet_finder/favoritesPet_IDsifter.dart';
+import 'package:cs222_final_project_pet_finder/favorites_pet_id_sifter.dart';
 import 'package:cs222_final_project_pet_finder/pet.dart';
 import 'package:cs222_final_project_pet_finder/pet_finder_parser.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-void main(){
-  final listIDs=[69316382,69316378];
+void main() {
+  final listIDs = [69316382, 69316378];
   final parser = PetFinderParser();
   final File petTestFile = File('test/apiResponse.json');
   final fileContents = petTestFile.readAsStringSync();
   final results = parser.parsePetInfo(fileContents);
-  final petSifter=FavPetIDSifter();
-  List<dynamic> finalPets=petSifter.findPetObjects(listIDs, results);
-  test('The objects returned is the right one',(){
-    Pet pet=finalPets[0];
-    int testedID=pet.petID;
-    expect(testedID,listIDs[0]);
+  final petSifter = FavPetIDSifter();
+  List<dynamic> finalPets = petSifter.findPetObjects(listIDs, results);
+  test('The objects returned is the right one', () {
+    Pet pet = finalPets[0];
+    int testedID = pet.petID;
+    expect(testedID, listIDs[0]);
   });
-  test('the names listed are Count Coolman and Salem',(){
-    String finalPetNames='';
-    for (var pet in finalPets){
-      finalPetNames+='${pet.name}';
+  test('the names listed are Count Coolman and Salem', () {
+    String finalPetNames = '';
+    for (var pet in finalPets) {
+      finalPetNames += '${pet.name}';
     }
-    expect(finalPetNames,'Count CoolmanSalem ');
+    expect(finalPetNames, 'Count CoolmanSalem ');
   });
 }
