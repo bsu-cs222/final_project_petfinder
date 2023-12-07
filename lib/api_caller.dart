c88be968919cadc14a0c5753448eb05d4d4dfc5d
TierraCodes <112350897+TierraCodes@users.noreply.github.com>
2023-12-07 08:58:52 -0500

    Refactor
    Renamed a lot of functions

--- lib/api_caller.dart
+++ lib/api_caller.dart
@@ -2,23 +2,22 @@ import 'package:cs222_final_project_pet_finder/query_builder.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 
-class APICaller {
+class ApiCaller {
   Future<Object> makeRequestToAPI(urlFinal) async {
     final query = QueryBuilder();
     final response = await http.post(
       Uri.parse('https://api.petfinder.com/v2/oauth2/token'),
-      body: await query.constructTokenQuery(
+      body: await query.pullQueryToken(
           dotenv.env['api_id'], dotenv.env['api_secret']),
     );
     if (response.statusCode == 200) {
       final queryResponse = await http.get(
         Uri.parse('$urlFinal'),
-        headers: query.petFinderCallBuilder(response),
+        headers: query.authenticateTokenHeader(response),
       );
       return (queryResponse.body);
     } else {
       throw Exception('Error: ${response.statusCode}');
     }
-    //We need to add exceptions.
   }
 }
--- lib/query_builder.dart
+++ lib/query_builder.dart
@@ -5,32 +5,23 @@ import 'package:cs222_final_project_pet_finder/input_evaluator.dart';
 import 'package:cs222_final_project_pet_finder/pet.dart';
 
 class QueryBuilder {
-  Future<Map<String, String>> constructTokenQuery(id, secret) async {
-    final Map<String, String> requestBody = {
+  Future<Map<String, String>> pullQueryToken(id, secret) async {
+    final Map<String, String> tokenResponse = {
       'grant_type': 'client_credentials',
       'client_id': '$id',
       'client_secret': '$secret',
     };
-    return (requestBody);
+    return (tokenResponse);
   }
 
-  Map<String, String> petFinderCallBuilder(tokenRequestResponse) {
-    final Map<String, dynamic> decodedTokenRequestResponse =
-        json.decode(tokenRequestResponse.body);
-    final String accessToken = decodedTokenRequestResponse['access_token'];
+  Map<String, String> authenticateTokenHeader(tokenResponse) {
+    final Map<String, dynamic> decodedTokenResponse =
+        json.decode(tokenResponse.body);
+    final String accessToken = decodedTokenResponse['access_token'];
     final Map<String, String> header = {
       'Authorization': 'Bearer $accessToken',
     };
     return (header);
-  }
-
-  String addFilter(Map filters, String url) {
-    for (final filter in filters.entries) {
-      if (filter.value != 'blank') {
-        url += '&${filter.key}=${filter.value}';
-      }
-    }
-    return url;
   }
 
   String pullAdoptionData(Pet pet, String status) {
@@ -43,6 +34,15 @@ class QueryBuilder {
     species = evaluator.evaluateSpeciesInput(species);
     final breed = pet.breed.replaceAll(' ', '-');
     return 'https://api.petfinder.com/v2/animals/?after=$formattedDate&status=$status&type=$species&breed=$breed&location=46241&distance=50';
+  }
+
+  String addFilter(Map filters, String url) {
+    for (final filter in filters.entries) {
+      if (filter.value != 'blank') {
+        url += '&${filter.key}=${filter.value}';
+      }
+    }
+    return url;
   }
 
   String pullBaseURL() {
